a6ef67169f86130e0840f731164d03b5a467e01b
domi <ltdomenicosg@gmail.com>
2021-09-15 18:45:35 +0200

    update

---
+++ script/counter.js
@@ -0,0 +1,70 @@
+var counter = {
+    "counter": 0,
+    "time": 10
+}
+var last_checktime = [0,0, 0, 0]
+var inner = [true, true, true, true]
+var time_timeout = 1000
+var time_delay = 500
+
+function exit_page(n) {
+    inner[n] = false
+    
+}
+function enter_page(n) {
+    inner[n] = true
+
+    last_checktime[n] =  get_counter("time") 
+    setTimeout(() => {
+        refresh(n)
+    }, time_timeout);
+}
+function refresh(n) {
+    
+    
+    setTimeout(() => {  check(n);    }, time_delay);
+    hit_counter("time");
+    setTimeout(() => {  if (inner[n]){refresh(n);    }  }, time_timeout);
+}
+
+
+function check(n) {
+    let time = get_counter("time") 
+    var difference = time - last_checktime[n]
+    
+
+    if (time > 300){
+        set_counter("time", -1)
+    }
+
+    if (difference > 0) {
+        console.log("PC "+n+ " > numero utenti: "+difference, counter)
+    }
+        
+
+
+        // difference sono gli utenti non più online
+        // RIMUOVERE!!!
+
+    // if(time > 20 && counter["counter"] == 1){
+    //     counter["time"] = 0
+    //     time = 0
+    // }
+    last_checktime[n] = time
+}
+
+
+function get_counter(c) {
+    return counter[c]
+}
+function hit_counter(c) {
+    counter[c]++;
+}
+function set_counter(c, n) {
+    counter[c] = n
+}
+
+function cl(t) {
+    console.log(t)
+}
+
--- script/script.js
+++ script/script.js
@@ -110,10 +110,12 @@ $(".counter").hide()
 var views = 0
 key_check =  "domescala.portfolio11_09_2021"
 
-console.log(localStorage.n_Visit10)
+console.log(localStorage.n_Visit11)
 
 console.log(localStorage.last_refresh)
 console.log(new Date().getTime())
+
+// localStorage.last_refresh = new Date().getTime();
 
 if(document.URL.split("portfoli")[1] == "o/?debug"){
 localStorage.last_refresh = new Date().getTime()
@@ -121,36 +123,139 @@ localStorage.last_refresh = new Date().getTime()
 
 console.log("response")
 date = new Date().getTime()
-if (!localStorage.last_refresh ){
-    console.log(localStorage.last_refresh);
+// if (localStorage.last_refresh == true ){
+    
+//     console.log("non prima visita");
+//     Counter("get")
+    
+//     }
+// if (new Date().getTime() - localStorage.last_refresh > (1000*3600)) {
+//     Counter("hit")
+//     localStorage.last_refresh = new Date().getTime()
+     
+//     }
+// else{
+//     console.log(localStorage.last_refresh);
 
+//     console.log("prima visita");
+//     Counter("hit")
+//     localStorage.last_refresh = new Date().getTime()    
+
+    
+//     }
+    console.log(localStorage.visit);
+if (localStorage.visit == "true"){
+    console.log("non prima visita");
+
+    if (new Date().getTime() - localStorage.last_refresh > (1000*3600)) {
+        Counter("hit")
+        localStorage.last_refresh = new Date().getTime()
+        }
+    else {
+        console.log("non prima visita");
+        Counter("get")
+    }
+}
+else {
     console.log("prima visita");
     Counter("hit")
     localStorage.last_refresh = new Date().getTime()    
-    }
-else if (new Date().getTime() - localStorage.last_refresh > (1000*3600)) {
-    Counter("hit")
-    localStorage.last_refresh = new Date().getTime()
-     
-    }
-else{
-
-    console.log("non prima visita");
-    Counter("get")
-
-    
-    }
+    localStorage.visit = true
+}
 
 function Counter(metodo) {
-    $.getJSON("https://api.allorigins.win/get?url=" +
-        encodeURIComponent("http://api.countapi.xyz/"+metodo+"/"+key_check+"/visits?  callback=callbackName") + "&callback=?",
+    $.getJSON("http://api.countapi.xyz/"+metodo+"/"+key_check+"/visits?  callback=callbackName" + "&callback=?",
         function (data) {
-            console.log(data.contents)
-            views = data.contents.split(":")[1].split("}")[0]
+            //console.log(data.contents)
+            let views = data.value;
 
-            $(".counter p").html (views)
-            $(".counter").show()
-            console.log("dopo")
+            $(".counter .views p").html (data.value);
+            $(".counter").show();
+            console.log("richiesta fatta");
         });
 } 
 
+
+// -----    COUNTER  REAL TIME   -----
+
+var time_timeout =  2000
+var time_delay = 100
+
+let count_time = 0
+var last_time = 0
+
+var list_count = [1,1,1,1,1,1]
+
+var namespace = "domescala2021"
+var key = "countertime1"
+var key2 = "countertime2"
+on = true
+function s() {
+    on = false
+    
+}
+// get_timecount()
+$(".counter .realtime p").html("1");
+// timerefresh();
+ list_count = [1,1,1,1,1,1]
+
+
+function average(n) {
+    list_count.shift();
+    list_count.push(n);
+    let m = 0
+    list_count.forEach(e => {m += e });
+    console.log(list_count)
+    return parseInt(m/list_count.length)
+
+}
+var a = setInterval(() => {    refresh() }, 1000 * 30);
+refresh();
+var first = true;
+function refresh() {
+    let time_start = new Date().getTime()
+    $.getJSON(
+        "https://api.countapi.xyz/hit/"+namespace+"/"+key,
+            function (data) {
+
+                let count_time = data.value
+                difference = count_time - last_time
+
+                console.log("get", difference, data )
+                last_time = data.value 
+                if(first){ 
+                    difference = 1;
+                    $.getJSON(
+                        "https://api.countapi.xyz/hit/"+namespace+"/"+key,
+                            function (data) {
+                                difference = data.value
+                            }
+                    )
+                }
+                $(".counter .realtime p").html(difference);
+
+                if(data.value > 1000 && difference == 1){
+                    $.getJSON(
+                        "https://api.countapi.xyz/set/"+namespace+"/"+key+"?value=0",
+                            function (data) {});
+                }
+                first = false
+
+            });
+ 
+}
+
+function check(){
+    $.getJSON(
+        "https://api.countapi.xyz/get/"+namespace+"/"+key,
+            function (data) {
+                console.log("check get", new Date(), data.value, last_time)
+
+                let count_time = data.value
+                difference = count_time - last_time
+
+                $(".counter .realtime p").html(difference);
+                console.log("get", difference )
+                last_time = data.value 
+            });    
+}
