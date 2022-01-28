a2a22137dd66f423c206a947c4955b0b31c76b3f
domi <ltdomenicosg@gmail.com>
2022-01-28 22:06:09 +0100

    fix
    i tag, boom text improved, animation wide title of sections, fixed logo dimensions

--- script/animation-title.js
+++ script/animation-title.js
@@ -6,12 +6,15 @@ for (let index = 0; index < text_boom.innerHTML.length; index++) {
 }
 text_boom.innerHTML = ""
 
-
+var boomed = false;
 for (let index = 0; index < text.length; index++) {
     const element = document.createElement("SPAN")
     element.classList = "textboom t"+index
     element.innerHTML = text[index]
-    
+    $(element).click(function () {
+        if(!boomed){    boom() }
+        else{           sboom()}
+    });
     if (element.innerHTML != " ") {
         text_boom.appendChild(element)
     }
@@ -20,7 +23,7 @@ for (let index = 0; index < text.length; index++) {
     }
 }
 var doc_span = document.querySelectorAll(".textboom")
-var boomed = false;
+
 
 function boom() {
     for (let index = 0; index < doc_span.length; index++) {
@@ -29,11 +32,12 @@ function boom() {
             x: (Math.random()*1500 + 500) * (Math.random()*1 - 0.4),
             y: (Math.random()*3500 + 500)* (Math.random()*1 - 0.1),
             s: Math.random()*5 + 0.3 ,
-            r: Math.random()*360 + 720 ,
+            r: (Math.random()*360 + 720) * (Math.round(Math.random()) * 2 - 1 ) ,
         }
         element.style["transition-duration"] = "30s"
-
+        // element.style["filter"] = "blur("+rand.s*1.5+"px)"
         element.style["transform"] = "translate("+rand.x+"px, "+rand.y+"px) scale("+rand.s+") rotate("+rand.r+"deg)"
+        element.style["cursor"] = "grab"
     }
     console.log("boom")
     boomed = true;
@@ -46,14 +50,44 @@ function sboom() {
         element.style["transform"] = ""
         element.style["transition-duration"] = "5s"
         element.style["color"] = "unset"
-
+        element.style["cursor"] = "grabbing"
     boomed = false;
         
     });
 }
 
 
-$(".name").click(function () {
-    if(!boomed){    boom() }
-    else{           sboom()}
-});
+const title_wide = [
+    document.querySelector("#containerabout h2"),
+    document.querySelector("#containerportfolio h2"),
+    document.querySelector("#containerskills h2"),
+    document.querySelector(".social_title"),
+    document.querySelector(".contacts_title")
+]
+const title_wide_tf = [true, true, true, true, true ]
+
+var only_first_time = true;
+var only_first_timeB = true;
+document.addEventListener("scroll", function () {
+    // console.log(title_wide_tf)
+    for (let i = 0; i < title_wide.length; i++) {
+        var y = title_wide[i].getBoundingClientRect().y
+        
+        if ( title_wide_tf[i] && y < 600 && y > 350 ) {
+            // console.log("i",i, title_wide[i], y)
+            title_wide[i].classList.add ("wide_text_animation")
+            if (title_wide[i].innerHTML == "Followami") {
+                title_wide[i+1].classList.add ("wide_text_animation")
+            }
+            title_wide_tf[i] = false
+            return "" //ferma la funzione
+            console.log("ciao")
+        }
+        else if(!title_wide_tf[i]  && (y > 1000 || y < 0)){
+            title_wide[i].classList.remove ("wide_text_animation")
+            title_wide_tf[i] = true
+            
+        }
+
+    }
+})
